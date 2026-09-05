b3fec028db30ea542f94f3e48fc26777c68dd8fd
JoelJimenez <jj491097@gmail.com>
2026-09-05 13:56:13 -0500

    Conectar frontend con backend de Railway

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { FiCalendar } from "react-icons/fi"
 import { io } from "socket.io-client"
 import MatchScreen from "./MatchScreen"
 
-const socket = io("http://localhost:5000")
+const socket = io(fiestamatchbackend-production.up.railway.app)
 
 function Welcome({ goToCreate }) {
   return (
